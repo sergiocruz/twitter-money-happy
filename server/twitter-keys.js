b36fdc5a9aa04bc76c370359623be01fd23581e1
Sergio Cruz <hello@sergiocruz.me>
2016-03-01 23:13:57 -0500

    Removed files

--- server/twitter-keys.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-   consumer_key: '3bxCoTTG8hGTtif8CHcAvhmcQ',
-   consumer_secret: 'HiNIYlb4UR2vaHEcdUmqdBpSthoUytjswOcvTjzxHsvuxy5Nyv',
-   access_token_key: '2694022290-PEURZu65oxSrFgkTdPa7SIRjNZRlS2IkEi7joh2',
-   access_token_secret: '96qSluyRo9GGtxNTbGiy1BURuDP83bFS8lO4xwvzjzfEy'
-};
