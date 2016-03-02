527f4237a0165053caacc70a2006b62bea798bae
Sergio Cruz <hello@sergiocruz.me>
2016-03-01 22:19:47 -0500

    UI it up

--- client/app/controllers/main-ctrl.js
+++ client/app/controllers/main-ctrl.js
@@ -1,9 +1,21 @@
+const ColorManipulate = require('color-manipulate');
+const Color = require('color');
+
+
 module.exports = [
+  '$scope',
+  '$timeout',
   MainCtrl
 ]
 
-function MainCtrl() {
+function MainCtrl($scope, $timeout) {
   var vm = this;
+  vm.mix = mix;
+  vm.factor = 50;
+  vm.color = null;
+
+  var red = Color("#cb3837");
+  var green = Color("#8BCB37");
 
   activate();
 
@@ -11,6 +23,51 @@ function MainCtrl() {
 
   function activate() {
     console.log('activated')
+    mix();
   }
-  
+
+  /**
+   * Mixes colors
+   * @return {Void}
+   */
+  function mix() {
+
+    // Parses factor
+    vm.factor = parseInt(vm.factor);
+
+    // Does nothing if number is invalid
+    if (isNaN(vm.factor)) {
+      vm.factor = 0;
+      return;
+    }
+
+    // Produces new color
+    vm.color = ColorManipulate.mix(red.clone(), green.clone(), vm.factor / 100);
+
+    // Calculates feeling
+    calculateFeeling();
+  }
+
+  /**
+   * Calculates feeling
+   * @return {Void}
+   */
+  function calculateFeeling() {
+
+    if (vm.factor < 20) {
+      vm.feeling = 'I hate it';
+    } else if (vm.factor < 40) {
+      vm.feeling = 'I don\'t like it'
+    } else if (vm.factor < 60) {
+      vm.feeling = 'It\'s ok'
+    } else if (vm.factor < 80) {
+      vm.feeling = 'I like it'
+    } else {
+      vm.feeling = 'I love it'
+    }
+
+  }
+
+  $scope.$watch('vm.factor', mix)
+
 }
