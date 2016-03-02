d94bdde45e11166222112106b89749e125633306
Jeremy Privett <jeremy@jeremyprivett.com>
2016-03-01 20:38:30 -0500

    Somewhat working Twitter socket

--- server/app.js
+++ server/app.js
@@ -3,6 +3,8 @@ var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 var port = process.env.PORT || 4000;
+var twcfg = require('./twitter');
+var tw = require('node-tweet-stream')(twcfg);
 
 server.listen(port, function () {
   console.log('Server listening at port %d', port);
@@ -15,24 +17,13 @@ app.get('/', function(req, res) {
 // Routing
 app.use(express.static(__dirname + '/public'));
 
+tw.track('javascript');
 
-io.on('connection', function (socket) {
-
-  socket.on('agent.connect', function(agent, cb) {
-
-    agent.id = agents.length + 1;
-
-    console.log('connecting agent', agent);
-
-    whoami = agent;
-    whoamiType = 'agent';
-
-    agents.push(agent);
-    cb(agent);
-
-    console.log('all agents', agents);
-  });
-
-  
-
+tw.on('tweet', function(tweet) {
+    console.log(tweet);
+    io.emit('tweet', tweet);
 });
+
+/*io.on('connection', function (socket) {
+    
+});*/
---
+++ server/twitter.js
@@ -0,0 +1,6 @@
+module.exports = {
+   consumer_key: '3bxCoTTG8hGTtif8CHcAvhmcQ',
+   consumer_secret: 'HiNIYlb4UR2vaHEcdUmqdBpSthoUytjswOcvTjzxHsvuxy5Nyv',
+   token: '2694022290-PEURZu65oxSrFgkTdPa7SIRjNZRlS2IkEi7joh2',
+   token_secret: '96qSluyRo9GGtxNTbGiy1BURuDP83bFS8lO4xwvzjzfEy'
+};
