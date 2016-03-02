15ca62f5c4418f042c32ba5ff6365c7a5d6fc40b
Sergio Cruz <hello@sergiocruz.me>
2016-03-01 20:12:55 -0500

    bye sails

--- client/app/app.js
+++ client/app/app.js
@@ -1,4 +1,9 @@
 var angular = require('angular')
+var socket = require('socket.io-client')('http://localhost:4000');
+
+socket.on('connect', function(){
+  console.log('connected')
+});
 
 angular
   .module('app', [
